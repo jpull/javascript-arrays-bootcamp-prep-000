dc7d9203957a8f86dfb0910831b702d66b3b8d7c
Jerry P <jpull@jpull.com>
2018-02-12 18:17:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,13 +1,19 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array,element) {
-  [element, ...array]
+  return [element, ...array]
 }
 function destructivelyAddElementToBeginningOfArray(array,element) {
   array = [element, ...array]
+  return array
 }
 function addElementToEndOfArray (array,element) {
-    [...array, element]
+   reaturn  [...array, element]
+    return array
 }
 function destructivelyAddElementToEndOfArray (array,element) {
     array = [...array, element]
+    reaturn array
+}
+function accessElementInArray(array,index) {
+  return 
 }
