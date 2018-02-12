79790cad05603627860177e12ee933320e8c735b
Jerry P <jpull@jpull.com>
2018-02-12 18:13:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,4 +2,6 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array,element) {
   [element, ...array]
 }
-function destructivelyAddElementToBeginningOfArray
+function destructivelyAddElementToBeginningOfArray(array,element) {
+  array = [element, ...array]
+}
